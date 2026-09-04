1901bcf6163d19f6235518197c2450b3f4895491
Brotherjiggy <emmyjones813@gmail.com>
2026-09-04 14:14:02 -0400

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
    API CONFIGURATION
 ========================================================= */
 
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://upupway-ai.onrender.com/";
 
 
 /* =========================================================
